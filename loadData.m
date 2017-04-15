935758e2dc228788d9373e215e7c9c39c85be6d8
HyperSeb <sebastien.blondiau@me.com>
2017-04-15 22:30:46 +0200

    Changed function name wav to data

--- loadData.m
+++ loadData.m
@@ -1,12 +1,8 @@
 % fonction important un échantillon de voix
-function obs = loadWav(path)
+function obs = loadData(path)
 [sound,fs]= audioread(path);
 band = [700, 1400]/(fs/2);
 [B, A] = butter(2,band);
 sound = filter(B, A, sound);
-
-% mixgauss_init...
-
-% melfcc
 obs = melfcc(sound,fs,'numcep',17);
 end
