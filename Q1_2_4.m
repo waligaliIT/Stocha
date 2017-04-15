9e0e208e6382b7b86995b935f431bcb26a6c96c6
NatGr <nathan.2@live.be>
2017-04-15 16:29:10 +0200

    Update Q1_2_4.m

--- Q1_2_4.m
+++ Q1_2_4.m
@@ -1,4 +1,4 @@
-% Script repondant à la question 1.2.4
+% Script repondant Ã  la question 1.2.4
 clear all;
 DonneesP1;
 
@@ -7,27 +7,30 @@ A = hmm3.A;
 B = hmm3.B;
 initA = mk_stochastic(rand(2,2));
 initB = mk_stochastic(rand(2,3));
+% initA = A;
+% initB = B;
 
-% mat. random + 250
-length = 250;
+
+% 100
+length = 100;
 seqObs = genSeq(hmm3,length,nbreSeq);
 [estVitA1, estVitB1] = hmmtrain(seqObs, initA, initB, 'ALGORITHM', 'Viterbi', 'PSEUDOTRANSITIONS', ones(size(A))/10^7);
 [estBaumA1, estBaumB1] = hmmtrain(seqObs, initA, initB);
 
-% mat. random + 500
-length = 500;
+% 250
+length = 250;
 seqObs = genSeq(hmm3,length,nbreSeq);
 [estVitA2, estVitB2] = hmmtrain(seqObs, initA, initB, 'ALGORITHM', 'Viterbi', 'PSEUDOTRANSITIONS', ones(size(A))/10^7);
 [estBaumA2, estBaumB2] = hmmtrain(seqObs, initA, initB);
 
-% mat. random + 1000
-length = 1000;
+% 500
+length = 500;
 seqObs = genSeq(hmm3,length,nbreSeq);
 [estVitA3, estVitB3] = hmmtrain(seqObs, initA, initB, 'ALGORITHM', 'Viterbi', 'PSEUDOTRANSITIONS', ones(size(A))/10^7);
 [estBaumA3, estBaumB3] = hmmtrain(seqObs, initA, initB);
 
-% mat. random + 2000
-length = 2000;
+% 1000
+length = 1000;
 seqObs = genSeq(hmm3,length,nbreSeq);
 [estVitA4, estVitB4] = hmmtrain(seqObs, initA, initB, 'ALGORITHM', 'Viterbi', 'PSEUDOTRANSITIONS', ones(size(A))/10^7);
 [estBaumA4, estBaumB4] = hmmtrain(seqObs, initA, initB);
