13cd64995122e86ea96a932cf9b4a408cf182020
NatGr <nathan.2@live.be>
2017-04-15 16:25:41 +0200

    Update Q1_2_6.m

--- Q1_2_6.m
+++ Q1_2_6.m
@@ -1,13 +1,19 @@
-% script répondant à la question 1.2.6
+% script rÃ©pondant Ã  la question 1.2.6
 clear all;
 
-% taille différente avec le bon nombre d'états
+% taille diffÃ©rente avec le bon nombre d'Ã©tats
 precision_s1_4 = Q1_2_6_fct(30, 4);
 precision_s2_4 = Q1_2_6_fct(100, 4);
 precision_s3_4 = Q1_2_6_fct(200, 4);
 precision_s4_4 = Q1_2_6_fct(500, 4);
 
-% même taille avec 1 à 6 états
+% taille diffÃ©rente avec beaucoup d'Ã©tats
+precision_s1_6 = Q1_2_6_fct(30, 6);
+% precision_s2_6 = Q1_2_6_fct(100, 6); calculÃ© plus bas
+precision_s3_6 = Q1_2_6_fct(200, 6);
+precision_s4_6 = Q1_2_6_fct(500, 6);
+
+% mÃªme taille avec 1 Ã  6 Ã©tats
 precision_s2_1 = Q1_2_6_fct(100, 1);
 precision_s2_2 = Q1_2_6_fct(100, 2);
 precision_s2_3 = Q1_2_6_fct(100, 3);
