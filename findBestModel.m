54a9de9ac3aad714898eec7739559034fb2442d5
HyperSeb <sebastien.blondiau@me.com>
2017-04-18 16:12:17 +0200

    add computeScore

--- findBestModel.m
+++ findBestModel.m
@@ -1,7 +1,7 @@
 % function finding the most likely digit and the probabilities associated with all of them
 % models is a table containing the models for the digits from 0 to 9 in order,
 % being computed with the same number of spectral coefficients
-function [index, prob] = findBestModel(sound, models)
+function [digit, prob] = findBestModel(sound, models)
 
 
 data = getCoef(sound, size(models(1).mu, 1));
@@ -9,6 +9,6 @@ data = getCoef(sound, size(models(1).mu, 1));
 prob = arrayfun(@(m) mhmm_logprob(data, m.pi, m.A, m.mu, m.sigma, m.B), models)';
 
 [~, index] = max(prob);
-index = index - 1;
+digit = index - 1;
 
 end
