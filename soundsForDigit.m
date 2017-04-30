14f87559385bb81001ba3a0aa68d51117bb34666
Albatross <francoisdelarbre97@gmail.com>
2017-04-30 23:14:08 +0200

    soudsForDigit replace disp by warning and error & and use extractSoundFiles

--- soundsForDigit.m
+++ soundsForDigit.m
@@ -2,21 +2,22 @@
 function sounds = soundsForDigit(digit)
 %% check arguments
 if(digit < 0 || digit > 9)
-    disp ['Ceci n''est pas un chiffre: ' int2str(digit)]
-    return
+    error(['Ceci n''est pas un chiffre: ' int2str(digit)])
 end
 
 %% extract sound file from the directory
-givenDirectory = ['Examples/' int2str(digit) '/'];
-givenFiles = arrayfun(@(e) fullfile(givenDirectory, e.name), dir([givenDirectory 'ex-' int2str(digit) '-*.wav']), 'UniformOutput', false);
+givenDirectory = ['Examples/' int2str(digit)];
+givenFiles = extractSoundFiles( givenDirectory, digit, 'ex-', '-*' )
+%arrayfun(@(e) fullfile(givenDirectory, e.name), dir([givenDirectory 'ex-' int2str(digit) '-*.wav']), 'UniformOutput', false);
 
 files = givenFiles;
 
-otherDirectories = {'Data/seb/', 'Data/nathan/', 'Data/francois/'}; % directories holding everybody's data
+otherDirectories = {'Data/seb', 'Data/nathan', 'Data/francois'}; % directories holding everybody's data
 for i = 1:length(otherDirectories)
     tmpForMatlabLimitation = otherDirectories(i);
     otherDirectory = tmpForMatlabLimitation{1};
-    otherFiles = arrayfun(@(e) fullfile(otherDirectory, e.name), dir([otherDirectory int2str(digit) '-*.wav']), 'UniformOutput', false);
+    otherFiles = extractSoundFiles( otherDirectory, digit, '', '-*' );
+    %arrayfun(@(e) fullfile(otherDirectory, e.name), dir([otherDirectory int2str(digit) '-*.wav']), 'UniformOutput', false);
     files = [files; otherFiles];
 end
 
@@ -26,7 +27,7 @@ sounds = cell(numel(files), 1);
 for i = 1:numel(files)
 	[sounds{i}, fs] = audioread(files{i});
 	if fs ~= 44100
-		disp([files{i} 'is not sampled at 44100 Hz']);
+		warning([files{i} 'is not sampled at 44100 Hz']);
 	end
 end
 
